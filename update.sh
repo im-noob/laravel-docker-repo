274ad785d239c3d9a301bcafd709209da59c0982
aarav <aaravonly4you@gmail.com>
2022-01-13 09:29:34 +0530

    sql added

--- update.sh
+++
@@ -1,8 +0,0 @@
-docker-compose exec app git pull origin master
-docker-compose exec app composer install
-docker-compose exec app php artisan migrate
-docker-compose exec app php artisan cache:clear
-docker-compose exec app php artisan config:clear
-docker-compose exec app php artisan view:clear
-docker-compose exec app php artisan route:clear
-docker-compose exec app php artisan optimize
